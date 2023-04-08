7810e507febe6da6b5ce1d8586d890475f2440cc
azimust <woody.azimbek@gmail.com>
2023-04-08 15:07:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./components/hooks/usePosts";
+import axios from "axios";
+import PostService from "./components/API/PostService";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  useEffect(() => {
+    fetchPosts()
+  }, []);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+    setModal(false)
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  };
+
+  async function fetchPosts() {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyButton style={{marginTop: '20px'}} onClick={() => setModal(true)}>Создать пост</MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <hr style={{ margin: '15px 0' }}></hr>
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов 1' />
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import MyInput from './UI/input/MyInput'
+import MyButton from './UI/button/MyButton'
+
+const PostForm = (props) => {
+    const [post, setPost] = useState({title: '', body: ''});
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        const newPost = {
+            ...post, id: Date.now()
+        }
+        props.create(newPost)
+        setPost({title: '', body: ''})
+      }
+
+    return (
+        <form>
+            <MyInput
+                value={post.title}
+                onChange={e => setPost({ ...post, title: e.target.value })}
+                type='text'
+                placeholder={'Название поста'} />
+            <MyInput
+                value={post.body}
+                onChange={e => setPost({ ...post, body: e.target.value })}
+                type='text'
+                placeholder={'Описание поста'} />
+            <MyButton onClick={addNewPost}>Добавить пост</MyButton>
+        </form>
+    )
+}
+
+export default PostForm
