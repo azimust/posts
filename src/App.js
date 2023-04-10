5044ab6da4a89aa2d25b0d7e1ecd913e7b2b8728
azimust <woody.azimbek@gmail.com>
2023-04-10 17:45:56 +0600

    feat: add pagination

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./components/hooks/usePosts";
-import axios from "axios";
 import PostService from "./components/API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import { useFetching } from "./components/hooks/useFetching";
+import { getPageCount } from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
   const [posts, setPosts] = useState([]);
-
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+
+  const [fetchPosts, isPostLoading, postError] = useFetching(async (limit, page) => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit));
   })
 
   useEffect(() => {
-    fetchPosts()
+    fetchPosts(limit, page)
   }, []);
 
   const createPost = (newPost) => {
@@ -35,19 +41,25 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   };
 
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts(limit, page)
+  };
+
   return (
     <div className="App">
-      <MyButton style={{marginTop: '20px'}} onClick={() => setModal(true)}>Создать пост</MyButton>
+      <MyButton style={{ marginTop: '20px' }} onClick={() => setModal(true)}>Создать пост</MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
       <hr style={{ margin: '15px 0' }}></hr>
       <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && <h1 style={{textAlign: 'center'}}>{postError}</h1>}
+      {postError && <h1 style={{ textAlign: 'center' }}>{postError}</h1>}
       {isPostLoading
-      ? <div style={{display: "flex", justifyContent: "center", marginTop: '50px'}}><Loader/></div>
-      : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов 1' />
+        ? <div style={{ display: "flex", justifyContent: "center", marginTop: '50px' }}><Loader /></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов' />
       }
+      <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
     </div>
   )
 };
--- src/components/UI/MyModal/MyModal.jsx
+++ src/components/UI/MyModal/MyModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classes from './MyModal.module.css';
 
 const MyModal = ({ children, visible, setVisible }) => {
